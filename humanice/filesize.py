7b16510393bd23eabefb2545bbeaf33ea9bd017d
Tim Wedde <timwedde@icloud.com>
2019-09-29 03:01:53 +0200

    Move naturalfilesize to newer format method and support negative values

--- humanice/filesize.py
+++ humanice/filesize.py
@@ -10,7 +10,7 @@ suffixes = {
 }
 
 
-def naturalsize(value, binary=False, gnu=False, format='%.1f'):
+def naturalsize(value, binary=False, gnu=False, format='{:.1f}'):
     """Format a number of byteslike a human readable filesize (eg. 10 kB).  By
     default, decimal suffixes (kB, MB) are used.  Passing binary=true will use
     binary suffixes (KiB, MiB) are used and the base will be 2**10 instead of
@@ -24,22 +24,28 @@ def naturalsize(value, binary=False, gnu=False, format='%.1f'):
     else:
         suffix = suffixes['decimal']
 
+    value = str(value)
+    format_string = format
     base = 1024 if (gnu or binary) else 1000
+    if value.startswith("-"):
+        format_string = "-" + format_string
+        value = value.replace("-", "")
     bytes = float(value)
 
     if bytes == 1 and not gnu:
         return '1 Byte'
     elif bytes < base and not gnu:
-        return '%d Bytes' % bytes
+        return '{} Bytes'.format(int(bytes))
     elif bytes < base and gnu:
-        return '%dB' % bytes
+        return '{}B'.format(int(bytes))
 
     for i, s in enumerate(suffix):
         unit = base ** (i + 2)
         if bytes < unit and not gnu:
-            return (format + ' %s') % ((base * bytes / unit), s)
+            print(format_string + " {}", s)
+            return (format_string + " {}").format(base * bytes / unit, s)
         elif bytes < unit and gnu:
-            return (format + '%s') % ((base * bytes / unit), s)
+            return (format_string + "{}").format(base * bytes / unit, s)
     if gnu:
-        return (format + '%s') % ((base * bytes / unit), s)
-    return (format + ' %s') % ((base * bytes / unit), s)
+        return (format_string + "{}").format(base * bytes / unit, s)
+    return (format_string + " {}").format(base * bytes / unit, s)
